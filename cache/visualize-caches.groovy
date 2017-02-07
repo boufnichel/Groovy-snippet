38e1939056251abae630fcf4221108612ed1c483
Boufnichel mohamed <boufnichel.mohamed@gmail.com>
2017-02-07 11:26:24 +0000

    Update visualize-caches.groovy

--- cache/visualize-caches.groovy
+++ cache/visualize-caches.groovy
@@ -3,10 +3,12 @@ ehCacheBeanManagerName = "outizEhCacheManager";
 cacheManager = spring.getBean(ehCacheBeanManagerName)
 
 cacheManager.getCacheNames().each  {cacheName ->
- 
-  println cacheName
-  cache = cacheManager.getCache(cacheName).getNativeCache()
-  cache.getKeys().each {key ->
-    println cache.get(key)
-  }
+ // i don't want to display the native caches
+ if( cacheName != "entityCacheRegion" && cacheName != "queryCacheRegion" ) {
+   println cacheName
+   cache = cacheManager.getCache(cacheName).getNativeCache()
+   cache.getKeys().each {key ->
+     println cache.get(key)
+   }
+ }
 }
