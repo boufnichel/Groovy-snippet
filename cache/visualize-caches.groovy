455474adf3dc0bb21a99410a2054d37bd9cd09eb
Boufnichel mohamed <boufnichel.mohamed@gmail.com>
2017-02-07 11:26:49 +0000

    Update visualize-caches.groovy

--- cache/visualize-caches.groovy
+++ cache/visualize-caches.groovy
@@ -6,7 +6,7 @@ cacheManager.getCacheNames().each  {cacheName ->
  // i don't want to display the native caches
  if( cacheName != "entityCacheRegion" && cacheName != "queryCacheRegion" ) {
    println cacheName
-   cache = cacheManager.getCache(cacheName).getNativeCache()
+   cache = cacheManager.getCache(cacheName)
    cache.getKeys().each {key ->
      println cache.get(key)
    }
